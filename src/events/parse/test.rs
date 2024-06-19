86d2d6cd20c126517cd66c9d31e7d7dbb6b14f80
Thomas Loubiou <xabufr@gmail.com>
2024-06-19 23:50:28 +0200

    test: use test-case

--- src/events/parse/test.rs
+++ src/events/parse/test.rs
@@ -1,4 +1,5 @@
 use serde::Deserialize;
+use test_case::test_case;
 
 use super::from_str;
 
@@ -27,72 +28,17 @@ struct Variant6Data {
 #[derive(Deserialize, PartialEq, Debug)]
 struct Variant7Data;
 
-#[test]
-fn test_simple_variant_deser() {
-    test_successful_deserialization(r#"Variant1>>"#, MyEnum::Variant1)
-}
-
-#[test]
-fn test_variant_wrapper_deser() {
-    test_successful_deserialization(r#"Variant2>>toto"#, MyEnum::Variant2("toto".into()))
-}
-
-#[test]
-fn test_variant_tuple_deser() {
-    test_successful_deserialization(r#"Variant3>>1,toto"#, MyEnum::Variant3(1, "toto".into()))
-}
-
-#[test]
-fn test_variant_long_string_deser() {
-    test_successful_deserialization(
-        r#"Variant4>>titi,toto,tata"#,
-        MyEnum::Variant4("titi".into(), "toto,tata".into()),
-    )
-}
-
-#[test]
-fn test_variant_struct_deser() {
-    test_successful_deserialization(r#"Variant5>>"#, MyEnum::Variant5(Variant5Data {}))
-}
-
-#[test]
-fn test_variant_simple_struct_deser() {
-    test_successful_deserialization(
-        r#"Variant6>>2,test"#,
-        MyEnum::Variant6(Variant6Data {
-            id: 2,
-            name: "test".into(),
-        }),
-    )
-}
-
-#[test]
-fn test_variant_empty_struct_deser() {
-    test_successful_deserialization(r#"Variant7>>"#, MyEnum::Variant7(Variant7Data))
-}
-
-#[test]
-fn test_variant_struct_variant_deser() {
-    test_successful_deserialization(
-        r#"Variant8>>45,test2"#,
-        MyEnum::Variant8 {
-            id: 45,
-            name: "test2".into(),
-        },
-    )
-}
-
-#[test]
-fn test_bool_true() {
-    test_successful_deserialization(r#"VariantBool>>1"#, MyEnum::VariantBool(true))
-}
-
-#[test]
-fn test_bool_false() {
-    test_successful_deserialization(r#"VariantBool>>0"#, MyEnum::VariantBool(false))
-}
-
-fn test_successful_deserialization(s: &str, expected: MyEnum) {
+#[test_case("Variant1>>", MyEnum::Variant1)]
+#[test_case("Variant2>>test", MyEnum::Variant2("test".into()))]
+#[test_case("Variant3>>1,test", MyEnum::Variant3(1,"test".into()))]
+#[test_case("Variant4>>titi,toto,tata", MyEnum::Variant4("titi".into(),"toto,tata".into()))]
+#[test_case("Variant5>>", MyEnum::Variant5(Variant5Data{}))]
+#[test_case("Variant6>>2,test", MyEnum::Variant6(Variant6Data{id:2,name:"test".into()}))]
+#[test_case("Variant7>>", MyEnum::Variant7(Variant7Data))]
+#[test_case("Variant8>>45,test2", MyEnum::Variant8{id:45,name:"test2".into()})]
+#[test_case("VariantBool>>1", MyEnum::VariantBool(true) ; "True boolean")]
+#[test_case("VariantBool>>0", MyEnum::VariantBool(false); "False boolean")]
+fn test_successful_deserialization_cases(s: &str, expected: MyEnum) {
     let d: MyEnum = from_str(s).unwrap();
     assert_eq!(expected, d);
 }
